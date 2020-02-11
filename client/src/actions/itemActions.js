d292b31c52e83421a57bf6b8e8722915ac87638a
pawelsan <pawelsan@yahoo.com>
2020-02-11 23:06:33 +0100

    axios fetch works

--- client/src/actions/itemActions.js
+++ client/src/actions/itemActions.js
@@ -1,17 +1,27 @@
-import { GET_ITEMS, ADD_ITEM, DELETE_ITEM } from './types';
+import axios from 'axios';
+import { GET_ITEMS, ADD_ITEM, DELETE_ITEM, ITEMS_LOADING } from './types';
 
-export const getItems = () => {
+// dispatch come from thunk, enables us to make an asynchronous request
+export const getItems = () => dispatch => {
     // it goes back to the item Reducer and checking the action
     // when this action will reach itemReducer, it will launch the action.type part
     // so itemActions communicate with itemReducer without formal import here
     // the communicate on the level of the ShoppingList component where:
     // 1) this itemActions is directly imported
     // 2) the redux is connected to react via connect; redux already has itemReducers via reducers/index.js via store
-
-    return {
-        // this type is then used as action.type in switch in itemReducer
-        type: GET_ITEMS
-    }
+    dispatch(setItemsLoading());
+    // we added a proxy in package.json in React that enables it to have the url shortened
+    axios
+        .get('api/items')
+        .then(res =>
+            dispatch({
+                type: GET_ITEMS,
+                payload: res.data
+            }))
+    // return {
+    //     // this type is then used as action.type in switch in itemReducer
+    //     type: GET_ITEMS
+    // }
 };
 // deleteItem takes in the id because it needs to know which one to delete
 export const deleteItem = id => {
@@ -27,5 +37,11 @@ export const addItem = item => {
         // this type is then used as action.type in switch in itemReducer
         type: ADD_ITEM,
         payload: item
+    }
+}
+
+export const setItemsLoading = () => {
+    return {
+        type: ITEMS_LOADING
     }
 }
--- client/src/reducers/itemReducer.js
+++ client/src/reducers/itemReducer.js
@@ -1,21 +1,20 @@
 // Actual state is going to go here, and here we will be checking actions
 // In the reducer we have to evaluate action types
 import uuid from 'uuid';
-import { GET_ITEMS, ADD_ITEM, DELETE_ITEM } from '../actions/types';
+import { GET_ITEMS, ADD_ITEM, DELETE_ITEM, ITEMS_LOADING } from '../actions/types';
 
 const initialState = {
-    items: [
-        { id: uuid(), name: 'first item' },
-        { id: uuid(), name: 'second item' },
-        { id: uuid(), name: 'another item' }
-    ]
+    items: [],
+    loading: false
 }
 
 export default function (state = initialState, action) {
     switch (action.type) {
         case GET_ITEMS:
             return {
-                ...state
+                ...state,
+                items: action.payload,
+                loading: false
             };
         case DELETE_ITEM:
             return {
@@ -29,6 +28,11 @@ export default function (state = initialState, action) {
                 ...state,
                 // action.payload is the item from itemActions
                 items: [action.payload, ...state.items]
+            };
+        case ITEMS_LOADING:
+            return {
+                ...state,
+                loading: true
             };
         default:
             return state;
